d69a17de37b3eb4737c27d14c87d17fd36d34976
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2020-01-01 17:44:34 +0300

    Fix interval units.

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,6 +1,8 @@
+import { MS_PER_SEC } from '@/constants/units';
+
 /**
  * Used to obtain public key on server startup, so user token on login can be verified.
  */
 export const AUTH_LOGIN_SERVER_KEY_URL = 'https://login.airmash.online/key';
 
-export const AUTH_LOGIN_SERVER_DOWNLOAD_RETRY_INTERVAL_SEC = 5;
+export const AUTH_LOGIN_SERVER_DOWNLOAD_RETRY_INTERVAL_SEC = 5 * MS_PER_SEC;
