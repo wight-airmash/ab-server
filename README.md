a7888ecd3455e2c234030038d8aa1fdc72718565
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2020-01-10 01:57:23 +0300

    Implement minimap ideas.

--- README.md
+++ README.md
@@ -1,101 +1,16 @@
-# ab-server
+# Experimental branch: fully invisible prowlers (by xplay)
 
-Game server.
+Finished changes:
 
-Features:
+- Invisible prowlers aren't shown on mimimap. Starmash prowler radar doesn't work (actually without protocol changes it is broken).
+- Invisible prowlers see nobody on mimimap.
 
-- [in-game commands](./docs/commands.md).
+Not implemented yet:
 
-## Installation
+- Limit invisibility time somehow.
 
-Requirements:
+Also not tested with bots.
 
-- Node.js 12 (works on older versions too, but compatibility isn't guaranteed).
-- uWebSockets.js is C++ lib and distributed in binary files for the most popular operation systems, so it is unlikely, but you may have to compile it from source if you use an unpopular OS.
+## Known bugs
 
-Development:
-
-```sh
-npm i
-cp .env.example .env
-```
-
-Set `SU_PASSWORD` variable value.
-
-Build
-
-```sh
-npm run build:dev
-```
-
-or watch
-
-```sh
-npm run watch
-```
-
-And start
-
-```sh
-npm run start
-```
-
-## Settings
-
-Please see .env.example.
-
-## User accounts
-
-The current implementation does not support accounts synchronization between server instances.
-
-User accounts data are stored in `./data/user-stats.json` and in memory during the server run. Set `USER_ACCOUNTS` to `false` to turn user accounts off.
-
-Each user session is signed by the [login server](https://github.com/airmash-refugees/airmash-backend) (Ed25519). Validation uses the public key, which is downloaded when the server starts. To change the default key server URL set `AUTH_LOGIN_SERVER_KEY_URL` variable value (only https is supported).
-
-## Moderation Panel
-
-The server exports a `/admin` URL to allow privileged players to moderate the
-game, and a public log to allow everyone to see moderation actions.
-
-Add passwords to `admin/passwords.txt` like so:
-
-```
-wight:somepasswordforwight
-```
-
-The username is only used to enter a name into the log.
-
-Set `MODERATION_PANEL` to `false` to turn moderation panel off. To change panel URL set `MODERATION_PANEL_URL_ROUTE` variable value (`admin` by default).
-
-## "How to run" examples
-
-### Docker
-
-.env.production is default .env file for docker image.
-
-1. Build an image.
-   `docker build -t airbattle-server .`
-2. Run. Don't forget to pass envirounment variables and mount the volumes you need. Example:
-
-   `docker run -v /host/logs:/app/logs -v /host/data:/app/data -p 3501:3501 -e SERVER_TYPE=FFA -e SU_PASSWORD=mypass airbattle-server`
-
-   If you want to use TLS also mount `/app/certs` to the host directory with `privkey.pem` and `fullchain.pem`, and set ENDPOINTS_TLS=true.
-
-### Run directly or behind the proxy
-
-```sh
-cp .env.production .env
-npm i
-npm run build
-npm prune --production
-node ./dist/app.js
-```
-
-or use some kind of process managers.
-
-If you run the server behind the proxy it is important to pass the real IP to the server (`X-Real-IP` or `X-Forwarded-For` header).
-
-## Contribution
-
-1. Server performance is important. Some bots (like https://github.com/username2234/airmash_bot) generate many packets espesially in `-follow me` mode, you can use them to test.
-2. There are eslint rules here, please don't forget about this.
+Invisible prowlers are shown as spectating in starmash.
--- src/server/modes/base/responses/broadcast/score-board.ts
+++ src/server/modes/base/responses/broadcast/score-board.ts
@@ -1,5 +1,5 @@
-import { SERVER_PACKETS, ServerPackets } from '@airbattle/protocol';
-import { PLAYERS_ALIVE_STATUSES } from '@/constants';
+import { ServerPackets, SERVER_PACKETS } from '@airbattle/protocol';
+import { PLAYERS_ALIVE_STATUSES, SHIPS_TYPES } from '@/constants';
 import { BROADCAST_SCORE_BOARD, CONNECTIONS_SEND_PACKET } from '@/events';
 import { System } from '@/server/system';
 import { MainConnectionId } from '@/types';
@@ -25,16 +25,15 @@ export default class ScoreBoardBroadcast extends System {
   onScoreBoard(connectionId: MainConnectionId = null): void {
     const playersData = [];
     const players = [];
-    const rankings = [];
-    let recipients = null;
-
-    if (connectionId !== null) {
-      recipients = connectionId;
-    } else {
-      recipients = [...this.storage.mainConnectionIdList];
-    }
+    const sightedRankings = [];
+    const blindRankings = [];
+    const sightedRecipients = [];
+    const blindRecipients = [];
 
     this.storage.playerList.forEach(player => {
+      const isBlind =
+        player.planetype.current === SHIPS_TYPES.PROWLER && player.planestate.stealthed === true;
+
       playersData.push({
         alive: player.alivestatus.current,
         id: player.id.current,
@@ -42,8 +41,27 @@ export default class ScoreBoardBroadcast extends System {
         level: player.level.current,
         x: player.position.lowX,
         y: player.position.lowY,
+        isBlind,
       });
+
+      if (this.storage.playerMainConnectionList.has(player.id.current)) {
+        const playerConnectionId = this.storage.playerMainConnectionList.get(player.id.current);
+
+        if (connectionId !== null && playerConnectionId !== connectionId) {
+          return;
+        }
+
+        if (isBlind === true) {
+          blindRecipients.push(playerConnectionId);
+        } else {
+          sightedRecipients.push(playerConnectionId);
+        }
+      }
     });
+
+    if (blindRecipients.length === 0 && sightedRecipients.length === 0) {
+      return;
+    }
 
     playersData.sort((p1, p2) => p2.score - p1.score);
 
@@ -54,21 +72,37 @@ export default class ScoreBoardBroadcast extends System {
         level: playersData[i].level,
       } as ServerPackets.ScoreBoardData);
 
-      rankings.push({
-        id: playersData[i].id,
-        x: playersData[i].alive === PLAYERS_ALIVE_STATUSES.ALIVE ? playersData[i].x : 0,
-        y: playersData[i].alive === PLAYERS_ALIVE_STATUSES.ALIVE ? playersData[i].y : 0,
-      } as ServerPackets.ScoreBoardRanking);
+      if (playersData[i].isBlind === false) {
+        sightedRankings.push({
+          id: playersData[i].id,
+          x: playersData[i].alive === PLAYERS_ALIVE_STATUSES.ALIVE ? playersData[i].x : 0,
+          y: playersData[i].alive === PLAYERS_ALIVE_STATUSES.ALIVE ? playersData[i].y : 0,
+        } as ServerPackets.ScoreBoardRanking);
+      }
     }
 
-    this.emit(
-      CONNECTIONS_SEND_PACKET,
-      {
-        c: SERVER_PACKETS.SCORE_BOARD,
-        data: players,
-        rankings,
-      } as ServerPackets.ScoreBoard,
-      recipients
-    );
+    if (sightedRecipients.length !== 0) {
+      this.emit(
+        CONNECTIONS_SEND_PACKET,
+        {
+          c: SERVER_PACKETS.SCORE_BOARD,
+          data: players,
+          rankings: sightedRankings,
+        } as ServerPackets.ScoreBoard,
+        sightedRecipients
+      );
+    }
+
+    if (blindRecipients.length !== 0) {
+      this.emit(
+        CONNECTIONS_SEND_PACKET,
+        {
+          c: SERVER_PACKETS.SCORE_BOARD,
+          data: players,
+          rankings: blindRankings,
+        } as ServerPackets.ScoreBoard,
+        blindRecipients
+      );
+    }
   }
 }
