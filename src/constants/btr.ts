c0872ed1ee558ddc21002b485ffa83777007522e
congratulatio <45519482+congratulatio@users.noreply.github.com>
2020-01-26 17:57:37 +0000

    Add player spawn zones for BTR

--- src/constants/btr.ts
+++ src/constants/btr.ts
@@ -1,1 +1,15 @@
-export const BTR_WIN_ALERT_DURATION_SEC = 7;
+export const BTR_WIN_ALERT_DURATION_SEC = 13;
+
+export const BTR_SPAWN_WAITING = {
+  MIN_X: -120,
+  MIN_Y: -4500,
+  MAX_X: 2960,
+  MAX_Y: -960,
+};
+
+export const BTR_SPAWN_MATCH_START = {
+  MIN_X: -13384,
+  MIN_Y: -6192,
+  MAX_X: 13384,
+  MAX_Y: 6192,
+};
