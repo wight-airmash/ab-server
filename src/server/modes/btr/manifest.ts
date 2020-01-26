bba269234dbbde2a8f5f760c91472e59b9a7625a
congratulatio <45519482+congratulatio@users.noreply.github.com>
2020-01-26 18:58:29 +0000

    Replace respawn command in BTR, for error response

--- src/server/modes/btr/manifest.ts
+++ src/server/modes/btr/manifest.ts
@@ -1,6 +1,8 @@
 import BaseGameManifest from '@/server/modes/base/mainfest';
 import ServerCustomBroadcast from '@/server/modes/btr/responses/broadcast/server-custom';
 import Match from '@/server/components/game/match';
+import RespawnCommandHandler from '../base/commands/respawn';
+import BTRRespawnCommandHandler from './maintenance/respawn';
 import GameFirewallBroadcast from './responses/broadcast/game-firewall';
 import GameMatches from './maintenance/matches';
 
@@ -8,10 +10,19 @@ export default class BTRGameManifest extends BaseGameManifest {
   constructor({ app }) {
     super({ app });
 
+    const RespawnCommandSystem = [...this.app.systems].find(
+      system => system.constructor === RespawnCommandHandler
+    );
+
+    this.app.stopSystem(RespawnCommandSystem);
+
     this.systems = [
       // Broadcast.
       ServerCustomBroadcast,
       GameFirewallBroadcast,
+
+      // Commands.
+      BTRRespawnCommandHandler,
 
       // Maintenance.
       GameMatches,
