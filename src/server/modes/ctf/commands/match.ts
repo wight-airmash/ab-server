730241c79f162e8a18dde2bca01d6eb17c199b75
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2020-02-02 16:29:40 +0300

    Reduce code duplication.

--- src/server/modes/ctf/commands/match.ts
+++ src/server/modes/ctf/commands/match.ts
@@ -1,5 +1,6 @@
 import { BROADCAST_CHAT_SERVER_WHISPER, COMMAND_MATCH } from '@/events';
 import { System } from '@/server/system';
+import { unixMsToHumanReadable } from '@/support/datetime';
 import { ConnectionId } from '@/types';
 
 export default class MatchCommandHandler extends System {
@@ -19,30 +20,10 @@ export default class MatchCommandHandler extends System {
     const connection = this.storage.connectionList.get(connectionId);
 
     if (command === '') {
-      this.log.debug(`Player id${connection.meta.playerId} checked match info.`);
-
-      const matchDuration = Date.now() - this.storage.gameEntity.match.start;
-      const humanTimeParts = [];
-      const seconds = Math.floor((matchDuration / 1000) % 60);
-      const minutes = Math.floor((matchDuration / (1000 * 60)) % 60);
-      const hours = Math.floor((matchDuration / (1000 * 60 * 60)) % 24);
-
-      if (hours > 0) {
-        humanTimeParts.push(`${hours}h`);
-      }
-
-      if (minutes > 0) {
-        humanTimeParts.push(`${minutes}m`);
-      }
-
-      if (seconds > 0) {
-        humanTimeParts.push(`${seconds}s`);
-      }
-
       this.emit(
         BROADCAST_CHAT_SERVER_WHISPER,
         connection.meta.playerId,
-        `Match time: ${humanTimeParts.join(' ')}.`
+        `Match time: ${unixMsToHumanReadable(this.storage.gameEntity.match.start)}.`
       );
     }
   }
