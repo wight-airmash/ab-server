794befb8fe285eb63eda7c49ef0c234ccf1348a3
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2020-02-04 18:04:21 +0300

    Refactor /su command. Change log level.

--- src/server/modes/base/commands/su.ts
+++ src/server/modes/base/commands/su.ts
@@ -1,7 +1,7 @@
-import { COMMAND_SU, BROADCAST_CHAT_SERVER_WHISPER, RESPONSE_COMMAND_REPLY } from '@/events';
+import { LIMITS_SU, LIMITS_SU_WEIGHT } from '@/constants';
+import { BROADCAST_CHAT_SERVER_WHISPER, COMMAND_SU, RESPONSE_COMMAND_REPLY } from '@/events';
 import { System } from '@/server/system';
 import { MainConnectionId } from '@/types';
-import { LIMITS_SU_WEIGHT, LIMITS_SU } from '@/constants';
 
 export default class SuperuserCommandHandler extends System {
   constructor({ app }) {
@@ -13,11 +13,14 @@ export default class SuperuserCommandHandler extends System {
   }
 
   onCommandReceived(connectionId: MainConnectionId, password: string): void {
-    if (!this.storage.connectionList.has(connectionId)) {
+    const connection = this.storage.connectionList.get(connectionId);
+
+    if (
+      !this.storage.connectionList.has(connectionId) ||
+      !this.helpers.isPlayerConnected(connection.meta.playerId)
+    ) {
       return;
     }
-
-    const connection = this.storage.connectionList.get(connectionId);
 
     if (connection.meta.limits.su > LIMITS_SU) {
       this.emit(RESPONSE_COMMAND_REPLY, connectionId, 'Too frequent requests.');
@@ -27,17 +30,15 @@ export default class SuperuserCommandHandler extends System {
 
     connection.meta.limits.su += LIMITS_SU_WEIGHT;
 
-    if (connection && password === this.app.config.suPassword) {
-      const player = this.storage.playerList.get(connection.meta.playerId);
-
-      this.emit(
-        BROADCAST_CHAT_SERVER_WHISPER,
-        connection.meta.playerId,
-        'You have superuser rights now.'
-      );
-      this.log.debug(`Player id${connection.meta.playerId} became superuser.`);
+    if (password === this.app.config.suPassword) {
+      const { playerId } = connection.meta;
+      const player = this.storage.playerList.get(playerId);
 
       player.su.current = true;
+
+      this.emit(BROADCAST_CHAT_SERVER_WHISPER, playerId, 'You have superuser rights now.');
+
+      this.log.info(`Player id${playerId} became superuser.`);
     }
   }
 }
